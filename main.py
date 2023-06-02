c4f3e858ae7a498da78b09df8b922bb8a6fbee07
AeroAtCU <ian.adler@colorado.edu>
2023-06-02 06:41:18 +0000

    I did it all backwards

--- main.py
+++ main.py
@@ -17,16 +17,25 @@ def phi(x, k=0, D=1, summation_a=1):
 
     return phi_of_x
 
-# print(mcclarren.trapezoid(phi(lambda x:x, k=10), a=-3.0, b=3.0, pieces=1)) # How not to use lambdas:
-# replace 'lambda' with 'make a new temp function that takes one argument x. it's definition is:
-print(mcclarren.trapezoid(lambda x: phi(x, k=10), a=-3.0, b=3.0, pieces=1))
-print(mcclarren.trapezoid(lambda x: phi(x, k=1), a=-3.0, b=3.0, pieces=1))
-print(mcclarren.trapezoid(lambda x: phi(x, k=2), a=-3.0, b=3.0, pieces=1))
-print(mcclarren.trapezoid(lambda x: phi(x, k=4), a=-3.0, b=3.0, pieces=1))
+def phi_solution(x):
+    return 0.5*math.e**-x
 
 
+# Just to make things shorter. See bottom for lambda explain
+def trap_phi(k, a, b, pieces):
+    return mcclarren.trapezoid(lambda x: phi(x, k), a, b, pieces)
+
+# We should expect this to 'hone in' on a value as it adds more pieces
+trap_vary_pieces = [trap_phi(0.1, -3, 3, pieces) for pieces in [1, 10, 50, 100, 256]]
+for val in trap_vary_pieces: print (val)
+
+print(trap_phi(0.1, 0, 1, 256))
+print(phi_solution(1))
+
+# return mcclarren.trapezoid(lambda x: phi(x, k=4), a=-3.0, b=3.0, pieces=1)
 # Think of it like replacing this:
-# lambda x: phi(x, k=10)
+# `lambda x: phi(x, k=10)`
 # with just `lambda`
 # and then previously having this definition
-# def lambda(x): phi(x, k=10)
+# `def lambda(x): phi(x, k=10)`
+# In fact don't think about it that way, it's equivalent
